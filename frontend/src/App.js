87d4176db24f3b9937cfb9e70593acc83262e937
AlexanderBeard1997 <139777499+AlexanderBeard1997@users.noreply.github.com>
2023-09-14 15:07:36 +0100

    random price

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { faker } from "@faker-js/faker";
 
 function App() {
   const [gameInfo, setGameInfo] = useState({
     title: "",
     thumbnail: "",
+    price: "",
   });
 
   useEffect(() => {
@@ -13,22 +15,28 @@ function App() {
 
   const fetchGameData = async () => {
     try {
-      
       const response = await fetch("https://www.freetogame.com/api/game?id=452");
       if (!response.ok) {
         throw new Error("Game not found");
       }
       const data = await response.json();
 
+      // Use Faker.js to generate a random price
+      const randomPrice = formatPrice(faker.datatype.float({ min: 20, max: 40 }));
 
-      
       setGameInfo({
         title: data.title,
         thumbnail: data.thumbnail,
+        price: randomPrice,
       });
     } catch (error) {
       alert(`Error fetching Game data: ${error.message}`);
     }
+  };
+
+  // Function to format a number as a price with £ symbol
+  const formatPrice = (price) => {
+    return `£${price.toFixed(2)}`;
   };
 
   return (
@@ -38,6 +46,7 @@ function App() {
       <div>
         <div>
           <h1>Title: {gameInfo.title}</h1>
+          <p>Price: {gameInfo.price}</p>
           <img src={gameInfo.thumbnail} alt="img" />
         </div>
       </div>
