3ef14e8c427e573fc56b171c102f37a2b8a5d3de
AlexanderBeard1997 <139777499+AlexanderBeard1997@users.noreply.github.com>
2023-09-14 12:17:32 +0100

    update

---
+++ frontend/frontend/components/Navbar.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom'
+
+ 
+
+const Navbar = () => {
+
+    return (
+
+        <div id='navbar'>
+
+            <header>
+
+                <div>
+
+                    <Link to='/'>
+
+                        Home
+
+                    </Link>
+
+                    <Link to='/cart'>
+
+                        Cart
+
+                    </Link>
+
+                </div>
+
+            </header>
+
+        </div>
+
+    )
+
+}
+
+ 
+
+export default Navbar
